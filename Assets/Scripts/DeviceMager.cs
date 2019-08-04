4e86a773a18f95eb78f36ef1cb23c77a3572b028
school-wuhahah <1589313619@qq.com>
2019-08-04 22:23:20 +0800

    update 添加jar包 实现简单的Unity与Android交互

---
+++ Assets/Scripts/DeviceMager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceMager : Singleton<DeviceMager>
+{
+    private AndroidJavaClass javaDev;
+    private IntPtr classId;
+
+    public override void Init()
+    {
+        javaDev = new AndroidJavaClass("com.example.hyz.devlib.Device");
+        classId = javaDev.GetRawClass();
+        //DevInit();
+    }
+
+    //private void DevInit()
+    //{
+    //    Debug.Log("DevInit bgn ...");
+    //    AndroidJavaClass unityActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    //    AndroidJavaObject mContext = unityActivity.GetStatic<AndroidJavaObject>("currentActivity");
+    //    IntPtr methodid = AndroidJNIHelper.GetMethodID(classId, "onCreate", "(Ljava/lang/Object;)V", true);
+    //    if (methodid != IntPtr.Zero)
+    //    {
+    //        AndroidJNI.CallStaticVoidMethod(classId, methodid, null);
+    //        Debug.Log("DevInit callfunc ...");
+    //    }
+    //    Debug.Log("DevInit end ...");
+    //}
+
+    public string GetVersionName()
+    {
+        IntPtr methodid = AndroidJNIHelper.GetMethodID(classId, "getVersionName", "()Ljava/lang/String", true);
+        if (methodid != IntPtr.Zero)
+        {
+            string ret = AndroidJNI.CallStaticStringMethod(classId, methodid, null);
+            return ret;
+        }
+        return "";
+    }
+
+    public int GetVersionCode()
+    {
+        IntPtr methodid = AndroidJNIHelper.GetMethodID(classId, "getVersionCode", "()I", true);
+        if (methodid != IntPtr.Zero)
+        {
+            int ret = AndroidJNI.CallStaticIntMethod(classId, methodid, null);
+            return ret;
+        }
+        return 0;
+    }
+
+    public string GetAppName()
+    {
+        IntPtr methodid = AndroidJNIHelper.GetMethodID(classId, "getAppName", "()Ljava/lang/String", true);
+        if (methodid != IntPtr.Zero)
+        {
+            string ret = AndroidJNI.CallStaticStringMethod(classId, methodid, null);
+            return ret;
+        }
+        return "";
+    }
+
+    public override void Dispose()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/TestDev.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDev : MonoBehaviour
+{
+    public Text vnTxt;
+    public Text vcTxt;
+    public Text anTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TestDev bgn ...");
+#if UNITY_ANDROID
+        vnTxt.text = DeviceMager.instance.GetVersionName();
+        vcTxt.text = DeviceMager.instance.GetVersionCode().ToString();
+        anTxt.text = DeviceMager.instance.GetAppName();
+#endif
+        Debug.Log("TestDev end ...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
